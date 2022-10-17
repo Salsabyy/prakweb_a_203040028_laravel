82e834f7f275b3018d8e23ae7524c376b9b54577
Salsabyy <203040028.salsabila@mail.unpas.ac.id>
2022-10-17 19:36:53 +0700

    menyelasaikan vidio 3

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/Home', function () {
+    return view ('home');
 });
+
+Route::get('/about', function () {
+    return view ('about', [
+        "name" => "Salsabila Firdausi",
+        "email" => "salsabilafirdausi711@gmail.com",
+        "image" => "salsa.1.jpg"
+
+    ]);
+});
+
+Route::get('/posts', function () {
+    return view ('posts');
+});
+
