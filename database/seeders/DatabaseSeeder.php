6bb4dddf644f308e23c4554530937fcaed9d60f0
Salsabyy <203040028.salsabila@mail.unpas.ac.id>
2022-10-26 23:42:48 +0700

    menyelesaikan vidio 10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,19 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//\App\Models\User::factory(10)->create();
 
-        User::create([
-            'name' => 'Salsabila Firdausi',
-            'email' => 'salsabilafirdausi711@gmail.com',
-            'password' => bcrypt('171122')
-        ]);
 
-        User::create([
-            'name' => 'Bintang Yandi Putra',
-            'email' => 'acut121@gmail.com',
-            'password' => bcrypt('171122')
-        ]);
+      //  User::create([
+        //    'name' => 'Salsabila Firdausi',
+          //  'email' => 'salsabilafirdausi711@gmail.com',
+            //'password' => bcrypt('171122')
+        //]);
+
+     //   User::create([
+       //     'name' => 'Bintang Yandi Putra',
+         //   'email' => 'acut121@gmail.com',
+           // 'password' => bcrypt('171122')
+        //]);
+
+        User::factory(3)->create();
+
         Category::create([
             'name' => 'Web Programming',
             'slug' => 'web-programming'
@@ -39,41 +42,43 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi accusamus id cumque, sed dolores, tempora in vitae ipsa earum distinctio porro quod et? Deserunt quam impedit delectus quo eum in ea, nobis facilis recusandae ut magni esse quasi blanditiis necessitatibus maxime temporibus ullam deleniti modi sapiente. Ratione error atque minus, veniam numquam officiis impedit dignissimos cupiditate, hic sint at, quaerat sapiente cumque laborum nisi itaque nostrum! Consequatur tenetur maiores, repellat nostrum saepe nam! Consectetur distinctio, tempora excepturi numquam assumenda, doloremque maxime, iusto ex esse corrupti quisquam consequuntur temporibus eos culpa magni!',
-            'category_id' => 1,
-            'user_id' => 1
+        Post::factory(20)->create();
 
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi accusamus id cumque, sed dolores, tempora in vitae ipsa earum distinctio porro quod et? Deserunt quam impedit delectus quo eum in ea, nobis facilis recusandae ut magni esse quasi blanditiis necessitatibus maxime temporibus ullam deleniti modi sapiente. Ratione error atque minus, veniam numquam officiis impedit dignissimos cupiditate, hic sint at, quaerat sapiente cumque laborum nisi itaque nostrum! Consequatur tenetur maiores, repellat nostrum saepe nam! Consectetur distinctio, tempora excepturi numquam assumenda, doloremque maxime, iusto ex esse corrupti quisquam consequuntur temporibus eos culpa magni!',
+        //     'category_id' => 1,
+        //     'user_id' => 1
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi accusamus id cumque, sed dolores, tempora in vitae ipsa earum distinctio porro quod et? Deserunt quam impedit delectus quo eum in ea, nobis facilis recusandae ut magni esse quasi blanditiis necessitatibus maxime temporibus ullam deleniti modi sapiente. Ratione error atque minus, veniam numquam officiis impedit dignissimos cupiditate, hic sint at, quaerat sapiente cumque laborum nisi itaque nostrum! Consequatur tenetur maiores, repellat nostrum saepe nam! Consectetur distinctio, tempora excepturi numquam assumenda, doloremque maxime, iusto ex esse corrupti quisquam consequuntur temporibus eos culpa magni!',
-            'category_id' => 1,
-            'user_id' => 1
+        // ]);
 
-        ]);
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi accusamus id cumque, sed dolores, tempora in vitae ipsa earum distinctio porro quod et? Deserunt quam impedit delectus quo eum in ea, nobis facilis recusandae ut magni esse quasi blanditiis necessitatibus maxime temporibus ullam deleniti modi sapiente. Ratione error atque minus, veniam numquam officiis impedit dignissimos cupiditate, hic sint at, quaerat sapiente cumque laborum nisi itaque nostrum! Consequatur tenetur maiores, repellat nostrum saepe nam! Consectetur distinctio, tempora excepturi numquam assumenda, doloremque maxime, iusto ex esse corrupti quisquam consequuntur temporibus eos culpa magni!',
-            'category_id' => 2,
-            'user_id' => 1
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi accusamus id cumque, sed dolores, tempora in vitae ipsa earum distinctio porro quod et? Deserunt quam impedit delectus quo eum in ea, nobis facilis recusandae ut magni esse quasi blanditiis necessitatibus maxime temporibus ullam deleniti modi sapiente. Ratione error atque minus, veniam numquam officiis impedit dignissimos cupiditate, hic sint at, quaerat sapiente cumque laborum nisi itaque nostrum! Consequatur tenetur maiores, repellat nostrum saepe nam! Consectetur distinctio, tempora excepturi numquam assumenda, doloremque maxime, iusto ex esse corrupti quisquam consequuntur temporibus eos culpa magni!',
+        //     'category_id' => 1,
+        //     'user_id' => 1
 
-        ]);
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi accusamus id cumque, sed dolores, tempora in vitae ipsa earum distinctio porro quod et? Deserunt quam impedit delectus quo eum in ea, nobis facilis recusandae ut magni esse quasi blanditiis necessitatibus maxime temporibus ullam deleniti modi sapiente. Ratione error atque minus, veniam numquam officiis impedit dignissimos cupiditate, hic sint at, quaerat sapiente cumque laborum nisi itaque nostrum! Consequatur tenetur maiores, repellat nostrum saepe nam! Consectetur distinctio, tempora excepturi numquam assumenda, doloremque maxime, iusto ex esse corrupti quisquam consequuntur temporibus eos culpa magni!',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi accusamus id cumque, sed dolores, tempora in vitae ipsa earum distinctio porro quod et? Deserunt quam impedit delectus quo eum in ea, nobis facilis recusandae ut magni esse quasi blanditiis necessitatibus maxime temporibus ullam deleniti modi sapiente. Ratione error atque minus, veniam numquam officiis impedit dignissimos cupiditate, hic sint at, quaerat sapiente cumque laborum nisi itaque nostrum! Consequatur tenetur maiores, repellat nostrum saepe nam! Consectetur distinctio, tempora excepturi numquam assumenda, doloremque maxime, iusto ex esse corrupti quisquam consequuntur temporibus eos culpa magni!',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aspernatur sit error ullam, quos dolore deleniti veritatis excepturi accusamus id cumque, sed dolores, tempora in vitae ipsa earum distinctio porro quod et? Deserunt quam impedit delectus quo eum in ea, nobis facilis recusandae ut magni esse quasi blanditiis necessitatibus maxime temporibus ullam deleniti modi sapiente. Ratione error atque minus, veniam numquam officiis impedit dignissimos cupiditate, hic sint at, quaerat sapiente cumque laborum nisi itaque nostrum! Consequatur tenetur maiores, repellat nostrum saepe nam! Consectetur distinctio, tempora excepturi numquam assumenda, doloremque maxime, iusto ex esse corrupti quisquam consequuntur temporibus eos culpa magni!',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
         }
 }
